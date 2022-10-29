3a4e0adc0053bdd62be211a06773683b1f31e7b8
ivan <ivan1016017@gmail.com>
2022-10-29 11:19:15 -0500

    adding project

--- api/models/models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-
-
-db = SQLAlchemy()
-
-class OneMinuteFraud(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
